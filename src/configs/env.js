7ab7c8aa4246a4961498bb5794f93375f5ba31c4
sergeimishurin <sergei.mishurin88@gmail.com>
2018-01-16 11:57:24 +0400

    synced sessions storage with login reducers

--- src/configs/env.js
+++ src/configs/env.js
@@ -3,6 +3,5 @@ const API_DEV_URI = "http://api.gravitybrain.homestead/1.0";
 const API_PROD_URI = "http://gravitybrain.api.myemo.am/1.0";
 
 export const env = {
-    // API_URI: process.env.NODE_ENV === 'production' ? API_DEV_URI : API_PROD_URI,
-    API_URI: API_DEV_URI,
+    API_URI: process.env.NODE_ENV === 'production' ? API_DEV_URI : API_PROD_URI,
 };
