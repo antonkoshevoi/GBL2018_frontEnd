d16688bebdbd302c2b2455ccc1993dc4839a9c43
Arthur Alagulyan <arturalagulyan@gmail.com>
2018-01-19 03:40:47 +0400

    g

--- src/components/pages/homerooms/HomeroomForm.js
+++ src/components/pages/homerooms/HomeroomForm.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { FormControl, FormHelperText, Input, InputLabel, MenuItem, Select, Typography, Tab, Tabs, Paper } from 'material-ui';
+import { FormGroup, FormControlLabel } from 'material-ui/Form';
+import Checkbox from 'material-ui/Checkbox';
+import { getSchoolTeachers, getSchoolStudents } from "../../../redux/homerooms/actions";
+import {selectGetSchoolStudentsRequest, selectGetSchoolTeachersRequest} from "../../../redux/homerooms/selectors";
 
 function TabContainer(props) {
   return (
@@ -22,13 +27,48 @@ class HomeroomForm extends Component {
     errors: PropTypes.any
   };
 
-  state = {
-    value: 0,
+  constructor (props) {
+    super(props);
+    this.state = {
+      schoolTeachers: [],
+      schoolStudents: [],
+      activeTab: 0,
+      studentIds: [],
+    };
+  }
+
+  handleChangeTab = (event, activeTab) => {
+    this.setState({ activeTab });
   };
 
-  handleChangeTab = (event, value) => {
-    this.setState({ value });
-  };
+  componentDidMount() {
+    if (this.props.homeroom.id) {
+      this.props.getSchoolTeachers(this.props.homeroom.schoolId);
+      this.props.getSchoolStudents(this.props.homeroom.schoolId);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const schoolTeachers = this.props.getSchoolTeacherRequest.get('records');
+    const nextschoolTeachers = nextProps.getSchoolTeacherRequest.get('records');
+
+    if (!schoolTeachers && nextschoolTeachers) {
+      this.setState({
+        ...this.state,
+        schoolTeachers: nextschoolTeachers.toJS()
+      });
+    }
+
+    const schoolStudents = this.props.getSchoolStudentsRequest.get('records');
+    const nextschoolStudents = nextProps.getSchoolStudentsRequest.get('records');
+
+    if (!schoolStudents && nextschoolStudents) {
+      this.setState({
+        ...this.state,
+        schoolStudents: nextschoolStudents.toJS()
+      });
+    }
+  }
 
   _handleInputChange(event) {
     const { name, type, value, checked } = event.target;
@@ -37,6 +77,30 @@ class HomeroomForm extends Component {
       ...this.props.homeroom,
       [name]: value
     });
+  }
+
+  _handleStudentsCheckboxChange(event) {
+    const { value } = event.target;
+    const index = this.state.studentIds.indexOf(value);
+
+    if (index < 0) {
+        this.state.studentIds.push(value);
+    } else {
+        this.state.studentIds.splice(index, 1);
+    }
+
+    this.props.onChange({
+        ...this.props.homeroom,
+        homeroomStudents: this.state.studentIds
+    });
+  }
+
+  _handleSchoolChange(event) {
+    const { value } = event.target;
+
+    this.props.getSchoolTeachers(value);
+    this.props.getSchoolStudents(value);
+    this._handleInputChange(event);
   }
 
   _renderSchools() {
@@ -49,21 +113,41 @@ class HomeroomForm extends Component {
     ));
   }
 
+  _renderTeacher(teacher, key) {
+    return <MenuItem key={teacher.id} value={ teacher.id }>
+      { teacher.firstName } { teacher.lastName }
+    </MenuItem>
+  }
+
+  _renderStudent(student, key) {
+    return <FormControlLabel key={student.id}
+      control={
+        <Checkbox
+          name='homeroomStudents'
+          checked={this.state.studentIds.indexOf(student.id) > 0}
+          onChange={ (e) => {this._handleStudentsCheckboxChange(e) }}
+          value={"3"}
+        />
+      }
+      label={student.firstName + ' ' + student.lastName}
+    />
+  }
+
   render() {
     const { homeroom, errors } = this.props;
-    const { value } = this.state;
+    const { activeTab } = this.state;
 
     return (
       <div className='row'>
 
           <Paper>
-            <Tabs value={value} onChange={this.handleChangeTab} centered>
+            <Tabs value={activeTab} onChange={this.handleChangeTab} centered>
               <Tab label="Details" />
               <Tab label="Administration" />
               <Tab label="Students" />
             </Tabs>
 
-          {value === 0 && <TabContainer>
+          {activeTab === 0 && <TabContainer>
             <div className="col-sm-8 m-auto">
             <FormControl aria-describedby='name-error-text' className='full-width form-inputs'>
               <InputLabel htmlFor='name-error'>Name</InputLabel>
@@ -98,58 +182,77 @@ class HomeroomForm extends Component {
             <FormControl aria-describedby='name-error-text' className='full-width form-inputs'>
               <InputLabel htmlFor='name-error'>Enrollment Start Date</InputLabel>
               <Input
-                  name='enrollment_startDate'
+                  name='enrollmentStartDate'
                   margin='dense'
                   fullWidth
-                  value={homeroom.enrollment_startDate || ''}
+                  value={homeroom.enrollmentStartDate || ''}
                   onChange={(e) => { this._handleInputChange(e) }}/>
-                {errors && errors.get('enrollment_startDate') && <FormHelperText error>{ errors.get('enrollment_startDate').get(0) }</FormHelperText>}
+                {errors && errors.get('enrollmentStartDate') && <FormHelperText error>{ errors.get('enrollmentStartDate').get(0) }</FormHelperText>}
             </FormControl>
             <FormControl aria-describedby='name-error-text' className='full-width form-inputs'>
               <InputLabel htmlFor='name-error'>Enrollment End Date</InputLabel>
               <Input
-                  name='enrollment_endDate'
+                  name='enrollmentEndDate'
                   margin='dense'
                   fullWidth
-                  value={homeroom.enrollment_endDate || ''}
+                  value={homeroom.enrollmentEndDate || ''}
                   onChange={(e) => { this._handleInputChange(e) }}/>
-                {errors && errors.get('enrollment_endDate') && <FormHelperText error>{ errors.get('enrollment_endDate').get(0) }</FormHelperText>}
+                {errors && errors.get('enrollmentEndDate') && <FormHelperText error>{ errors.get('enrollmentEndDate').get(0) }</FormHelperText>}
             </FormControl>
             </div>
           </TabContainer>}
-          {value === 1 && <TabContainer>
+          {activeTab === 1 && <TabContainer>
             <div className="col-sm-8 m-auto">
             <FormControl className='full-width form-inputs'>
               <InputLabel htmlFor='name-error'>School</InputLabel>
               <Select
                   primarytext=""
                   name='schoolId'
-                  onChange={(e) => { this._handleInputChange(e) }}
-                  children={this._renderSchools()}
+                  onChange={(e) => { this._handleSchoolChange(e) }}
                   value={homeroom.schoolId || ''}>
+                <MenuItem value={null} primarytext="Select School"/>
+                {this._renderSchools()}
               </Select>
               {errors && errors.get('schoolId') && <FormHelperText error>{ errors.get('schoolId').get(0) }</FormHelperText>}
             </FormControl>
             <FormControl className='full-width form-inputs'>
               <InputLabel htmlFor='name-error'>Teacher</InputLabel>
               <Select
-                  primarytext=""
+                  primarytext="Select Teacher"
                   name='teacherId'
                   onChange={(e) => { this._handleInputChange(e) }}
                   value={homeroom.teacherId || ''}>
                 <MenuItem value={null} primarytext=""/>
+                {this.state.schoolTeachers.map((teacher, key) => {
+                  return this._renderTeacher(teacher, key)
+                })}
               </Select>
                 {errors && errors.get('teacherId') && <FormHelperText error>{ errors.get('teacherId').get(0) }</FormHelperText>}
             </FormControl>
             </div>
           </TabContainer>}
-          {value === 2 && <TabContainer>
-            Students
+          {activeTab === 2 && <TabContainer>
+            <FormGroup row>
+                {this.state.schoolStudents.map((student, key) => {
+                    return this._renderStudent(student, key)
+                })}
+            </FormGroup>
           </TabContainer>}
           </Paper>
       </div>
     );
   }
 }
+
+HomeroomForm = connect(
+    (state) => ({
+        getSchoolTeacherRequest: selectGetSchoolTeachersRequest(state),
+        getSchoolStudentsRequest: selectGetSchoolStudentsRequest(state),
+    }),
+    (dispatch) => ({
+        getSchoolTeachers: (schoolId) => { dispatch(getSchoolTeachers(schoolId)) },
+        getSchoolStudents: (schoolId) => { dispatch(getSchoolStudents(schoolId)) },
+    })
+)(HomeroomForm);
 
 export default HomeroomForm;
