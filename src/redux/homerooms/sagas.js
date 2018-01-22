04e985802a1016e4afed8ab51ab58fc197d5cf98
art_vaz <art.vazigeghtsyan@gmail.com>
2018-01-22 11:22:37 +0400

    fix

--- src/redux/homerooms/sagas.js
+++ src/redux/homerooms/sagas.js
@@ -1,14 +1,9 @@
 import { all } from 'redux-saga/effects';
 import {
-<<<<<<< HEAD
   CREATE_FAIL, CREATE_SUCCESS, GET_RECORDS_FAIL, GET_SCHOOLS_FAIL,
   GET_SINGLE_RECORD_FAIL, UPDATE_FAIL, BULK_UPLOAD_SUCCESS,
   UPDATE_SUCCESS, BULK_UPLOAD_FAIL
-=======
-  CREATE_FAIL, CREATE_SUCCESS, GET_RECORDS_FAIL,
-  GET_SINGLE_RECORD_FAIL, UPDATE_FAIL,
-  UPDATE_SUCCESS
->>>>>>> 38c57d5ba164720461cbe0b2c0311db1e624a054
+
 } from './actions';
 import { yieldErrorToasts, yieldSuccessToasts } from '../../helpers/utils';
 import i18n from '../../configs/i18n';
