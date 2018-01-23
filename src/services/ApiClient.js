02b8b704340699e3e43a095cc582a1642bed742a
sergeimishurin <sergei.mishurin88@gmail.com>
2018-01-23 10:46:05 +0400

    bulki

--- src/services/ApiClient.js
+++ src/services/ApiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import qs from 'qs';
-import { env } from '../configs/env'
+import { env } from '../configs/.env'
 import SessionStorage from './SessionStorage';
 
 export default class ApiClient
