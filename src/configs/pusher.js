752b31dad8e5f26cb12b24425d7e276ddeae327e
sergeimishurin <sergei.mishurin88@gmail.com>
2018-01-26 15:16:04 +0400

    moved pusher parameters to .env

--- src/configs/pusher.js
+++ src/configs/pusher.js
@@ -4,8 +4,8 @@ import SessionStorage from '../services/SessionStorage';
 
 Pusher.logToConsole = env.DEBUG;
 
-const pusher = new Pusher('76ec1d6846da76372695', {
-  cluster: 'ap2',
+const pusher = new Pusher(env.PUSHER_APP_KEY, {
+  cluster: env.PUSHER_CLUSTER,
   authEndpoint: `${env.API_URI}/broadcasting/auth`,
   auth: {
     headers: {
