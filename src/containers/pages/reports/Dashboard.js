2e9fef960faea6a57c52a720ffd6ac0f261edea6
Serhii Klymenko <klym.serhiy@gmail.com>
2018-04-29 23:29:54 +0300

    change UI of top block on 'reports' page

--- src/containers/pages/reports/Dashboard.js
+++ src/containers/pages/reports/Dashboard.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import {translate} from "react-i18next";
-import LineChart from "./widgets/LineChart";
-import PassRate from "./widgets/PassRate";
-import SchoolAverageChart from "./widgets/SchoolAverageChart";
-import RosterStatistic from "./widgets/RosterStatistic";
-import DashboardTabs from "./DashboardTabs";
+import {translate} from 'react-i18next';
+import LineChart from './widgets/LineChart';
+import PassRate from './widgets/PassRate';
+import SchoolAverageChart from './widgets/SchoolAverageChart';
+import RosterStatistic from './widgets/RosterStatistic';
+import DashboardTabs from './DashboardTabs';
 
 class Dashboard extends Component {
 
@@ -16,18 +16,27 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="fadeInLeft animated">
-        <div className="row">
-          <div className="col-sm-12 col-md-6 col-lg-3">
-            <RosterStatistic/>
+        <div className="row dashboard-main-top row-reports-main-top-block m-portlet  m-portlet--head-solid-bg">
+          <div className="m-portlet__head report-snapshot-header-border border-b-blue">
+            <div className="m-portlet__head-caption">
+              <div className="m-portlet__head-title">
+                <h3 className="m-portlet__head-text reports-text-header">Reports Snapshot</h3>
+              </div>
+            </div>
           </div>
-          <div className="col-sm-12 col-md-6 col-lg-3">
-            <LineChart/>
-          </div>
-          <div className="col-sm-12 col-md-6 col-lg-3">
-            <PassRate/>
-          </div>
-          <div className="col-sm-12 col-md-6 col-lg-3">
-            <SchoolAverageChart/>
+          <div className="reports-flex-row">
+            <div className="col-sm-12 col-md-6 col-lg-3">
+              <RosterStatistic/>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-3">
+              <LineChart/>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-3">
+              <PassRate/>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-3">
+              <SchoolAverageChart/>
+            </div>
           </div>
         </div>
         <div className="row">
