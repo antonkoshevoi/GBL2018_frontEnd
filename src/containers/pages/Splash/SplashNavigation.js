ea43e8680074f0eac033a6e92d13568803fb6a83
webdev498 <dpang989@mail.com>
2018-06-26 12:44:49 -0700

    Improve the mobile responsiveness on header

--- src/containers/pages/Splash/SplashNavigation.js
+++ src/containers/pages/Splash/SplashNavigation.js
@@ -3,23 +3,56 @@ import {NavLink} from 'react-router-dom';
 import LanguageSwitcher from "../../../components/ui/LanguageSwitcher";
 
 class SplashNavigation extends Component {
+
+  burgerToggle () {
+    let linksEl = document.querySelector('.narrow-links');
+    if (linksEl.style.display === 'block') {
+      linksEl.style.display = 'none';
+    }
+    else {
+      linksEl.style.display = 'block';
+    }
+  }
+
   render() {
     const {t} = this.props;
 
     return (
-      <div className="splash-navigation  m--hidden-mobile">
-        <div className="container text-center clearfix">
-          <NavLink to={`/about`} className="btn no-border m-btn btn-sm ">{t('about')}</NavLink>
-          <NavLink to={`/store`} className="btn no-border m-btn btn-sm ">{t('store')}</NavLink>
-          <NavLink to={`/parents`} className="btn no-border m-btn btn-sm ">{t('studentAndParents.title')}</NavLink>
-          <NavLink to={`/schools`} className="btn no-border m-btn btn-sm ">{t('schoolAndTeacher.title')}</NavLink>
-          <NavLink to={`//pubtool.gravitybrain.com/`} className="btn no-border m-btn btn-sm ">{t('publishers.title')}</NavLink>
+      <nav>
+        <div className="splash-navigation nav-wide">
+          <div className="container text-center clearfix wide-div">
+            <NavLink to={`/about`} className="btn no-border m-btn btn-sm ">{t('about')}</NavLink>
+            <NavLink to={`/store`} className="btn no-border m-btn btn-sm ">{t('store')}</NavLink>
+            <NavLink to={`/parents`} className="btn no-border m-btn btn-sm ">{t('studentAndParents.title')}</NavLink>
+            <NavLink to={`/schools`} className="btn no-border m-btn btn-sm ">{t('schoolAndTeacher.title')}</NavLink>
+            <NavLink to={`//pubtool.gravitybrain.com/`} className="btn no-border m-btn btn-sm ">{t('publishers.title')}</NavLink>
+            <div className="splash-navigation-tools">
+              <LanguageSwitcher/>
+            </div>
+          </div>
+        </div>
+        <div className="splash-navigation nav-narrow">
+          <div class="nav-narrow-collapse-container">
+            <i className="fa fa-bars fa-2x" onClick={this.burgerToggle}></i>
+            <div className="links text-right">
+              <NavLink to={`/login`} className="btn no-border m-btn btn-sm ">Login</NavLink>
+              <NavLink to={`/signup`} className="btn btn-bordered m-btn btn-sm signup">Sign up</NavLink>
+            </div>
+          </div>
           <div className="splash-navigation-tools">
             <LanguageSwitcher/>
           </div>
-        </div>
 
-      </div>
+          <div className="container text-center clearfix narrow-links">
+            <NavLink to={`/about`} className="btn no-border m-btn btn-sm ">{t('about')}</NavLink>
+            <NavLink to={`/store`} className="btn no-border m-btn btn-sm ">{t('store')}</NavLink>
+            <NavLink to={`/parents`} className="btn no-border m-btn btn-sm ">{t('studentAndParents.title')}</NavLink>
+            <NavLink to={`/schools`} className="btn no-border m-btn btn-sm ">{t('schoolAndTeacher.title')}</NavLink>
+            <NavLink to={`//pubtool.gravitybrain.com/`} className="btn no-border m-btn btn-sm ">{t('publishers.title')}</NavLink>
+            
+          </div>
+        </div>
+      </nav>
     )
   }
 }
