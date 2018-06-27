ab6cde1013e1f7a9d4a0d00316a9d43188867f47
webdev498 <dpang989@mail.com>
2018-06-27 12:35:12 -0700

    Update the images with new image urls and correct some styles

--- src/containers/pages/Splash/SplashPlatform.js
+++ src/containers/pages/Splash/SplashPlatform.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import spacer from '../../../media/images/svg/bl-spacer.svg';
-import squareFlipper from '../../../media/images/svg/square-flipper.svg';
-import books from '../../../media/images/svg/circle-books.svg';
-import shcool from '../../../media/images/svg/circle-school.svg';
-import parentChild from '../../../media/images/svg/circle-parent-child.svg';
 import flipperMap from  './platformContentMap.json'
+
+const spacerUrl = '//d2cnhr6egzpvdl.cloudfront.net/image/bl-spacer.70e5290c.svg';
+const circleBookUrl = '//d2cnhr6egzpvdl.cloudfront.net/image/circle-books.a4c524f8.svg';
+const circleParentChildUrl = '//d2cnhr6egzpvdl.cloudfront.net/image/circle-parent-child.ab68fd20.svg';
+const circleSchoolUrl = '//d2cnhr6egzpvdl.cloudfront.net/image/circle-school.1359579c.svg';
+
 const style = {
   flipper: {
     backgroundSize: '90%',
@@ -14,45 +15,61 @@ const style = {
   }
 }
 const flippers = [
-  {icon: parentChild },
-  {icon: shcool },
-  {icon: books },
+  {icon: circleParentChildUrl },
+  {icon: circleSchoolUrl },
+  {icon: circleBookUrl },
 ];
 
-const FlipperCards = props => {
-  const {t} = props;
-  return (
-    <div className="col-md-4 text-center" key={props.index} style={style.flipper}>
-      <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-        <div class="flipper">
-          <div class="front">
-            <img src={flippers[props.index].icon} alt="" className="flipperIcon"/>
-            <div className="flipper-title">
-              <span>{t(`${props.title}.title`)}</span>
+
+class FlipperCards extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handleTouchOnFlip = this.handleTouchOnFlip.bind(this);
+
+    this.state = { flipped : false };
+  }
+
+  handleTouchOnFlip() {
+    this.setState({ flipped : !this.state.flipped })
+  }
+
+  render() {
+    const {t} = this.props;
+    return (
+      <div className="col-md-4 text-center" key={this.props.index} style={style.flipper}>
+        <div className={`flip-container ${this.state.flipped ? 'hover' : ''}`} onTouchStart={this.handleTouchOnFlip}>
+          <div className="flipper">
+            <div className="front">
+              <img src={flippers[this.props.index].icon} alt="" className="flipperIcon"/>
+              <div className="flipper-title">
+                <span>{t(`${this.props.title}.title`)}</span>
+              </div>
             </div>
-          </div>
 
-          <div class="back">
-            <div className="flipper-cards">
-              <hr />
-              {props.content.map((items, index) =>
-                <div>
-                  <h5>{t(`${props.title}.${items.title}`)}</h5>
-                  <ul>
-                    {items.contents.map( cItems =>
-                      <li>{t(`${props.title}.${cItems}`)}</li>
-                    )}
-                  </ul>
-                </div>
+            <div className="back">
+              <div className="flipper-cards">
+                <hr />
+                {this.props.content.map((items, index) =>
+                  <div>
+                    <h5>{t(`${this.props.title}.${items.title}`)}</h5>
+                    <ul>
+                      {items.contents.map( cItems =>
+                        <li>{t(`${this.props.title}.${cItems}`)}</li>
+                      )}
+                    </ul>
+                  </div>
 
-              )}
-              <hr />
+                )}
+                <hr />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 
@@ -66,7 +83,7 @@ class SplashPlatform extends Component {
       <div className="splash-platform">
         <header className="header-break container text-center">
           <h2>{t('learningPlatform')}</h2>
-          <img src={spacer} alt="---=== ===---" width="200"/>
+          <img src={spacerUrl} alt="---=== ===---" width="200"/>
         </header>
         <div className="container">
           <div className="row">
