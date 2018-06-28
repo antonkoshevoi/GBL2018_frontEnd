c46b403585937b6ec93eb001ca26860a176d0c37
Pavel Kamenev <pkamenev@gmail.com>
2018-06-28 09:28:29 +0600

    pusher settings

--- src/configs/.env.js
+++ src/configs/.env.js
@@ -6,6 +6,6 @@ const API_PROD_URI = "https://api.gravitybrain.com/1.0";
 export const env = {
   DEBUG: process.env.NODE_ENV !== 'production',
   API_URI: process.env.NODE_ENV === 'production' ? API_PROD_URI : API_DEV_URI,
-  PUSHER_APP_KEY: process.env.NODE_ENV === 'production' ? '66a42eaeff65aab2a335' : '76ec1d6846da76372695',
-  PUSHER_CLUSTER: 'ap2'
+  PUSHER_APP_KEY: '8e6b7b90711c72e76633',
+  PUSHER_CLUSTER: 'us2'
 };
