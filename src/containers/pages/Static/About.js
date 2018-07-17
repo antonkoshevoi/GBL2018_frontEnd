0e9d7de7db37445beb350bc7bce60e9cd01ad62c
Pavel Kamenev <pkamenev@gmail.com>
2018-07-17 15:26:50 +0600

    static pages

--- src/containers/pages/Static/About.js
+++ src/containers/pages/Static/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {translate} from 'react-i18next';
 
 import SplashHeader from '../Splash/SplashHeader'
@@ -11,25 +11,46 @@ import StickyHeader from 'react-sticky-header';
 const minHeight = window.document.documentElement.clientHeight - 225;
 
 
-const About = (props) => {
-  return (<div className="splash">
-    <StickyHeader
-      // This is the sticky part of the header.
-      header={
-        <section>
-          <SplashHeader {...props} />
-          <SplashNavigation {...props} />
-        </section>
-      }
-    >
-      <section className="splash-container">
-        <div className="container" style={{minHeight: minHeight}}>
+class About extends Component {
+    
+  constructor(props) {
+    super(props);
+  }    
+  
+  _renderContent() {
+    const {t} = this.props;
+    const paragraphs = t('aboutContent.paragraphs', { returnObjects: true });
+        
+    return paragraphs.map((record, key) => (
+      <div>
+        <h2>{record.title}</h2>
+        <p>{record.content}</p>
+      </div>));
+  }
 
-        </div>
-        <SplashFooter {...props} />
-      </section>
-    </StickyHeader>
-  </div>)
+  render() {
+    const {t} = this.props;
+        
+    return (       
+        <div className="splash">
+          <StickyHeader            
+            header={
+                <section>
+                    <SplashHeader {...this.props} />
+                    <SplashNavigation {...this.props} />
+                </section>
+            }
+          >
+            <section className="splash-container">
+              <div className="container" style={{minHeight: minHeight}}>
+                  <p><img className="img-thumbnail" alt="About Us" src={t('aboutContent.image')} /></p>
+                  { this._renderContent() }
+              </div>
+              <SplashFooter {...this.props} />
+            </section>
+          </StickyHeader>
+        </div>);
+    }
 };
 
 export default withRouter(translate('splashScreen')(About));
--- src/containers/pages/Static/TermsService.js
+++ src/containers/pages/Static/TermsService.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {translate} from 'react-i18next';
 
 import SplashHeader from '../Splash/SplashHeader'
@@ -9,234 +9,50 @@ import '../Splash/splash.css';
 import 'react-sticky-header/styles.css';
 import StickyHeader from 'react-sticky-header';
 
+class TermsService extends Component {
+    
+  constructor(props) {
+    super(props);
+  }    
+  
+  _renderContent() {
+    const {t} = this.props;
+    
+    const paragraphs = t('termsAndConditions.paragraphs', {
+        interpolation: {escapeValue: false}, 
+        returnObjects: true
+    });       
+        
+    return paragraphs.map((record, key) => (           
+      <div>
+        <h4>{record.title}</h4>
+        <p>{record.content.split('\n').map(line => <p>{line.replace('{br}', '<br />')}</p>)}</p>
+      </div>));
+  }
 
-const TermsService = (props) => {
-  return (<div className="splash">
-    <StickyHeader
-      // This is the sticky part of the header.
-      header={
-        <section>
-          <SplashHeader {...props} />
-          <SplashNavigation {...props} />
-        </section>
-      }
-    >
-      <section className="splash-container">
-        <div className="container">
-          <h1 className="c19" id="h.ptax33dxfm6p">
-            <span className="c13">Terms and Conditions</span>
-          </h1>
-          <h2 className="c8" id="h.3os73nfxtgzj">
-            <span className="c7">Welcome to Gravity Brain Learning Inc.</span>
-          </h2>
-          <p className="c0">
-        <span className="c1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;These terms and conditions outline the rules and regulations for
-            the use of Gravity Brain Learning Inc.&#39;s Website. &nbsp;</span>
-          </p>
-          <p className="c0">
-            <span className="c1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Gravity Brain Learning Inc. is located at: </span>
-          </p>
-          <p className="c5">
-            <span className="c1">PO Box 224 </span>
-          </p>
-          <p className="c5">
-            <span className="c1">Montmartre, Saskatchewan</span>
-          </p>
-          <p className="c5">
-            <span className="c1">S0G3M0, </span>
-          </p>
-          <p className="c5">
-            <span className="c1">CANADA</span>
-          </p>
-          <p className="c0">
-        <span className="c1">By accessing this website we assume you accept these terms and conditions in full. Do not continue to use Gravity
-            Brain Learning Inc.&#39;s website if you do not accept all of the terms and conditions stated on this page.</span>
-          </p>
-          <p className="c0">
-        <span className="c1">The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and any
-            or all Agreements: &ldquo;Client&rdquo;, &ldquo;You&rdquo; and &ldquo;Your&rdquo; refers to you, the person accessing
-            this website and accepting the Company&rsquo;s terms and conditions. &ldquo;The Company&rdquo;, &ldquo;Ourselves&rdquo;,
-          &ldquo;We&rdquo;, &ldquo;Our&rdquo; and &ldquo;Us&rdquo;, refers to our Company. &ldquo;Party&rdquo;, &ldquo;Parties&rdquo;,
-            or &ldquo;Us&rdquo;, refers to both the Client and ourselves, or either the Client or ourselves. All terms refer
-            to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to
-            the Client in the most appropriate manner, whether by formal meetings of a fixed duration, or any other means,
-            for the express purpose of meeting the Client&rsquo;s needs in respect of provision of the Company&rsquo;s stated
-            services/products, in accordance with and subject to, prevailing law of Canada. Any use of the above terminology
-            or other words in the singular, plural, capitalisation and/or he/she or they, are taken as interchangeable and
-            therefore as referring to same.Cookies</span>
-          </p>
-          <p className="c0">
-        <span className="c1">We employ the use of cookies. By using Gravity Brain Learning Inc.&#39;s website you consent to the use of cookies
-            in accordance with Gravity Brain Learning Inc.&rsquo;s privacy policy.Most of the modern day interactive websites
-            use cookies to enable us to retrieve user details for each visit. Cookies are used in some areas of our site
-            to enable the functionality of this area and ease of use for those people visiting. Some of our affiliate partners
-            may also use cookies.</span>
-          </p>
-          <h2 className="c8" id="h.htn917tgrfhq">
-            <span className="c7">&nbsp;License</span>
-          </h2>
-          <p className="c0">
-        <span className="c1">Unless otherwise stated, Gravity Brain Learning Inc. and/or it&rsquo;s licensors own the intellectual property rights
-            for all material on Gravity Brain Learning Inc.. All intellectual property rights are reserved. You may view
-            and/or printpages from https://gravitybrain.com/ for your own personal use subject to restrictions set in these
-            terms and conditions.</span>
-          </p>
-          <p className="c0">
-            <span className="c1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You must not:</span>
-          </p>
-          <p className="c0">
-            <span className="c1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          </p>
-          <p className="c0">
-        <span className="c1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Republish material
-            from https://gravitybrain.com/</span>
-          </p>
-          <p className="c0">
-        <span className="c1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sell, rent or sub-license
-            material from https://gravitybrain.com/</span>
-          </p>
-          <p className="c0">
-        <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Reproduce, duplicate
-            or copy material from </span>
-            <span className="c4">
-            <a className="c2" href="https://www.google.com/url?q=https://gravitybrain.com/&amp;sa=D&amp;ust=1527749156861000">https://gravitybrain.com/</a>
-        </span>
-          </p>
-          <p className="c0 c20">
-            <span className="c1">Redistribute content from Gravity Brain Learning Inc. (unless content is specifically made for redistribution).</span>
-          </p>
-          <h2 className="c8" id="h.gz9crmeay65p">
-            <span className="c7">Hyperlinking to our Content</span>
-          </h2>
-          <p className="c0">
-            <span className="c1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          </p>
-          <p className="c0">
-            <span className="c1">The following organizations may link to our Web site without prior written approval:</span>
-          </p>
-          <p className="c0">
-        <span className="c1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Government
-            agencies;
-        </span>
-          </p>
-          <p className="c0">
-        <span className="c1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Search
-            engines;
-        </span>
-          </p>
-          <p className="c0">
-        <span className="c1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;News
-            organizations;
-        </span>
-          </p>
-          <p className="c0">
-        <span className="c1">Online directory distributors when they list us in the directory may link to our Web site in the same manner as they
-            hyperlink to the Web sites of other listed businesses; and System-wide Accredited Businesses except soliciting
-            non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our
-            Website.
-        </span>
-          </p>
-          <p className="c0">
-        <span className="c1">These organizations may link to our home page, to publications or to other Web site information so long as the link:
-            (a) is not in any way misleading; (b) does not falsely imply sponsorship, endorsement or approval of the linking
-            party and its products or services; and (c) fits within the context of the linking party&#39;s site.</span>
-          </p>
-          <p className="c0">
-        <span className="c1">We may consider and approve in our sole discretion other link requests from the following types of organizations:commonly-known
-            consumer and/or business information sources such as Chambers of Commerce, American Automobile Association, AARP
-            and Consumers Union; dot.com community sites; associations or other groups representing charities, including
-            charity giving sites, online directory distributors; internet portals; accounting, law and consulting firms whose
-            primary clients are businesses; and educational institutions and trade associations.</span>
-          </p>
-          <p className="c0">
-        <span className="c1">We will approve link requests from these organizations if we determine that: (a) the link would not reflect unfavorably
-            on us or our accredited businesses (for example, trade associations or other organizations representing inherently
-            suspect types of business, such as work-at-home opportunities, shall not be allowed to link); (b)the organization
-            does not have an unsatisfactory record with us; (c) the benefit to us from the visibility associated with the
-            hyperlink outweighs the absence of ; and (d) where the link is in the context of general resource information
-            or is otherwise consistent with editorial content in a newsletter or similar product furthering the mission of
-            the organization.</span>
-          </p>
-          <p className="c0">
-        <span className="c1">These organizations may link to our home page, to publications or to other Web site information so long as the link:
-            (a) is not in any way misleading; (b) does not falsely imply sponsorship, endorsement or approval of the linking
-            party and it products or services; and (c) fits within the context of the linking party&#39;s site.</span>
-          </p>
-          <p className="c0">
-        <span>If you are among the organizations listed in paragraph 2 above and are interested in linking to our website, you
-            must notify us by sending an e-mail to </span>
-            <span className="c4">
-            <a className="c2" href="mailto:admin@gravitybrain.com">admin@gravitybrain.com</a>
-        </span>
-            <span className="c1">. Please include your name, your organization name, contact information (such as a phone number and/or e-mail address)
-            as well as the URL of your site, a list of any URLs from which you intend to link to our Web site, and a list
-            of the URL(s) on our site to which you would like to link. Allow 2-3 weeks for a response.</span>
-          </p>
-          <h2 className="c8" id="h.l8e4l8ra0tdb">
-            <span className="c7">Approved organizations may hyperlink to our Web site as follows:</span>
-          </h2>
-          <ul className="c11 lst-kix_geqvtjo3j5t9-0 start">
-            <li className="c0 c14">
-              <span className="c1">By use of our corporate name; or</span>
-            </li>
-            <li className="c0 c14">
-              <span className="c1">By use of the uniform resource locator (Web address) being linked to; or</span>
-            </li>
-            <li className="c0 c14">
-            <span className="c1">By use of any other description of our Web site or material being linked to that makes sense within the context
-                and format of content on the linking party&#39;s site.</span>
-            </li>
-          </ul>
-          <p className="c0">
-        <span className="c1">No use of Gravity Brain Learning Inc.&rsquo;s logo or other artwork will be allowed for linking absent a trademark
-            license agreement.</span>
-          </p>
-          <h2 className="c8" id="h.4kp1llz6r5nw">
-            <span className="c7">Iframes</span>
-          </h2>
-          <p className="c0">
-        <span className="c1">Without prior approval and express written permission, you may not create frames around our Web pages or use other
-            techniques that alter in any way the visual presentation or appearance of our Web site.</span>
-          </p>
-          <h2 className="c8" id="h.n0qcuxxo7q8c">
-            <span className="c7">Reservation of Rights</span>
-          </h2>
-          <p className="c0">
-        <span className="c1">We reserve the right at any time and in its sole discretion to request that you remove all links or any particular
-            link to our Web site. You agree to immediately remove all links to our Web site upon such request. We also reserve
-            the right to amend these terms and conditions and its linking policy at any time. By continuing to link to our
-            Web site, you agree to be bound to and abide by these linking terms and conditions.</span>
-          </p>
-          <h2 className="c8" id="h.k8h7ocgdzhfo">
-            <span className="c7">Removal of links from our website</span>
-          </h2>
-          <p className="c0">
-        <span className="c1">If you find any link on our Web site or any linked web site objectionable for any reason, you may contact us about
-            this. We will consider requests to remove links but will have no obligation to do so or to respond directly to
-            you.
-        </span>
-          </p>
-          <p className="c0">
-        <span className="c1">Whilst we endeavour to ensure that the information on this website is correct, we do not warrant its completeness
-            or accuracy; nor do we commit to ensuring that the website remains available or that the material on the website
-            is kept up to date.</span>
-          </p>
-          <h2 className="c8" id="h.sym5hj9oymbd">
-            <span className="c7">Content Liability</span>
-          </h2>
-          <p className="c0">
-        <span className="c1">We shall have no responsibility or liability for any content appearing on your Web site. You agree to indemnify and
-            defend us against all claims arising out of or based upon your Website. No link(s) may appear on any page on
-            your Web site or within any context containing content or materials that may be interpreted as libelous, obscene
-            or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any
-            third party rights.</span>
-          </p>
-
-        </div>
-        <SplashFooter {...props} />
-      </section>
-    </StickyHeader>
-  </div>)
+  render() {
+    const {t} = this.props;
+        
+    return (       
+        <div className="splash">
+          <StickyHeader            
+            header={
+                <section>
+                    <SplashHeader {...this.props} />
+                    <SplashNavigation {...this.props} />
+                </section>
+            }
+          >
+            <section className="splash-container">
+              <div className="container">
+                  <h1>{t('termsAndConditions.title')}</h1>
+                  { this._renderContent() }
+              </div>
+              <SplashFooter {...this.props} />
+            </section>
+          </StickyHeader>
+        </div>);
+    }
 };
 
 export default withRouter(translate('splashScreen')(TermsService));
