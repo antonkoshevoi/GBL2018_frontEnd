03b9f17854c591b9b64bba308dc50377d9ae3f11
Pavel Kamenev <pkamenev@gmail.com>
2018-08-08 09:40:22 +0600

    Messages UI

--- src/components/messages/Messenger.js
+++ src/components/messages/Messenger.js
@@ -9,112 +9,105 @@ import MessageDivider from './MessageDivider';
 import {Scrollbars} from "react-custom-scrollbars";
 
 class Messenger extends Component {
-  static propTypes = {
-    thread: PropTypes.instanceOf(Immutable.Map).isRequired,
-    onNewMessage: PropTypes.func.isRequired,
-  };
+    static propTypes = {
+        thread: PropTypes.instanceOf(Immutable.Map).isRequired,
+        onNewMessage: PropTypes.func.isRequired,
+    };
 
-  state = {
-    newMessage: ''
-  };
+    state = {
+          newMessage: ''
+    };
+  
+    _scrollToBottom() {           
+        const { scrollbars } = this.refs;
+        scrollbars.scrollToBottom();        
+    }  
 
-  _scrollToBottom () {
-    let element = document.getElementById('messages-container');
-    element.scrollTop = element.scrollHeight;
-  }
+    _renderMessages () {
+        const { thread, userData, t } = this.props;
+        const messages = thread.get('messages');        
 
-  /**
-   * render messages by type
-   * @private
-   */
-  _renderMessages () {
-    const { thread, userData } = this.props;
-    const messages = thread.get('messages');
-    const loading = false;
+        if (messages.size === 0) {
+            return (
+                <MessageDivider>{t('noMessagesYet')}</MessageDivider>
+            );
+        }
 
-    if (!loading && messages.size === 0) {
-      return (
-        <MessageDivider>No messages yet</MessageDivider>
-      );
+        return messages.reverse().map((message, key) => {
+
+        if (message.get('type') === 'in') {
+            return <MessageIn
+                key={key}
+                body={message.get('body')}
+                username={message.get('user').get('username')}
+                color={message.get('user').get('color')}
+                avatar={message.get('user').get('avatar')}/>
+            }
+
+            if (message.get('type') === 'out') {
+                return <MessageOut
+                    key={key}
+                    avatar={userData.get('avatar')}
+                    body={message.get('body')}/>
+            }
+        });
+    };
+
+    _handleInputChange (e) {
+        this.setState({
+            newMessage: e.target.value
+        });
     }
 
-    return messages.reverse().map((message, key) => {
+    _handleSubmit (e) {
+        e.preventDefault();
 
-      if (message.get('type') === 'in') {
+        this.props.onNewMessage(this.state.newMessage);
+        this.setState({
+            newMessage: ''
+        });    
+    }
+  
+    componentDidMount() {
+      this._scrollToBottom();
+    }
 
-        return <MessageIn
-          key={key}
-          body={message.get('body')}
-          username={message.get('user').get('username')}
-          color={message.get('user').get('color')}
-          avatar={message.get('user').get('avatar')}/>
-      }
+    componentDidUpdate() {
+      this._scrollToBottom();
+    }
 
-        if (message.get('type') === 'out') {
-        return <MessageOut
-          key={key}
-          avatar={userData.get('avatar')}
-          body={message.get('body')}/>
-      }
-    });
-  };
+    render() {
+        const { t } = this.props;
+        return (
+            <div className="m-messenger m-messenger--message-arrow m-messenger--skin-light">
+              <div id='messages-container' className="m-messenger__messages mCS_7 mCS-autoHide" style={{height: '500px', position: 'relative', overflow: 'auto'}}>
+                <Scrollbars ref="scrollbars">
+                    { this._renderMessages() }
+                </Scrollbars>          
+              </div>
 
-  /**
-   *
-   * @param e
-   * @private
-   */
-  _handleInputChange (e) {
-    this.setState({
-      newMessage: e.target.value
-    });
-  }
+              <div className="m-messenger__seperator"></div>
 
-  /**
-   *
-   * @param e
-   * @private
-   */
-  _handleSubmit (e) {
-    e.preventDefault();
-
-    this.props.onNewMessage(this.state.newMessage);
-    this.setState({
-      newMessage: ''
-    });
-  }
-
-  render() {
-    return (
-      <div className="m-messenger m-messenger--message-arrow m-messenger--skin-light">
-        <div id='messages-container' className="m-messenger__messages mCS_7 mCS-autoHide" style={{height: '500px', position: 'relative', overflow: 'auto'}}>
-          <Scrollbars>
-              { this._renderMessages() }
-          </Scrollbars>
-        </div>
-
-        <div className="m-messenger__seperator"></div>
-
-        <form className="m-messenger__form" onSubmit={(e) => { this._handleSubmit(e); }}>
-          <div className="m-messenger__form-controls">
-            <textarea
-              value={this.state.newMessage}
-              onChange={(e) => { this._handleInputChange(e) }}
-              type="text"
-              name=""
-              rows={5}
-              placeholder="Type here..."
-              className="m-messenger__form-input"></textarea>
-          </div>
-          <div className="m-messenger__form-tools">
-            <a className="m-messenger__form-attachment" onClick={(e) => { this._handleSubmit(e); }}>
-              <i className="la la-send"></i>
-            </a>
-          </div>
-        </form>
-      </div>
-    );
-  }
+              <form className="m-messenger__form" onSubmit={(e) => { this._handleSubmit(e); }}>
+                <div className="m-messenger__form-controls">
+                  <textarea
+                    value={this.state.newMessage}
+                    onChange={(e) => { this._handleInputChange(e) }}
+                    type="text"
+                    name=""
+                    rows={4}
+                    placeholder={t('typeHere')}
+                    className="m-messenger__form-input"></textarea>
+                </div>
+                <div className="m-messenger__form-tools">
+                  <a className="m-messenger__form-attachment" onClick={(e) => { this._handleSubmit(e); }}>
+                    <i className="la la-send"></i>
+                  </a>
+                </div>
+              </form>
+            </div>
+        );
+    }
 }
 
-export default translate('messenger')(Messenger);
+export default translate('translations')(Messenger);
--- src/components/messages/UserBrowser.js
+++ src/components/messages/UserBrowser.js
@@ -2,67 +2,67 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MetronicSearchInput from '../ui/metronic/MetronicSearchInput';
 import { CircularProgress } from '@material-ui/core';
+import { translate } from 'react-i18next';
 import Immutable from "immutable";
 import {Scrollbars} from "react-custom-scrollbars";
 
 class UserBrowser extends Component {
-  static propTypes = {
-    users: PropTypes.instanceOf(Immutable.List).isRequired,
-    loading: PropTypes.bool,
-    onSearch: PropTypes.func.isRequired,
-    onUserSelect: PropTypes.func.isRequired
-  };
+    static propTypes = {
+        users: PropTypes.instanceOf(Immutable.List).isRequired,
+        loading: PropTypes.bool,
+        onSearch: PropTypes.func.isRequired,
+        onUserSelect: PropTypes.func.isRequired
+    };
 
-  _onUserSelect (id) {
-    this.props.onUserSelect(id);
-  }
-
-  _renderUsers () {
-    const { users, loading, thread } = this.props;
-    const activeUserName = (thread !== undefined) ? thread.get('user').get('username') : '';
-
-      if (!loading && users.size === 0) {
-      return (
-        <div>
-          <span className="m-list-search__result-item-text">Users not found</span>
-        </div>
-      );
+    _onUserSelect (id) {
+        this.props.onUserSelect(id);
     }
 
-    return users.map((user, key) => {
+    _renderUsers () {
+        const { users, loading, thread, t } = this.props;
+        const activeUserName = (thread !== undefined) ? thread.get('user').get('username') : '';
 
-      return (
-          <div className={`m-widget4__item pointer ${activeUserName === user.get('username') && 'active'}`} key={key}
-               onClick={() => { this._onUserSelect(user.get('id')) }}>
-              <div className="m-widget4__img m-widget4__img--pic">
-                  <img src={user.get('avatar')} alt={ user.get('username') }/>
-              </div>
-              <div className="m-widget4__info">
-                    <span className="m-widget4__title">
-                    { user.get('username') }
-                    </span><br/>
-                    <span className="m-widget4__sub">
-                    </span>
-              </div>
-          </div>       
-      );
-    });
-  }
+        if (!loading && users.size === 0) {
+            return (
+                <div>
+                    <span className="m-list-search__result-item-text">{t('usersNotFound')}</span>
+                </div>
+            );
+        }
+
+        return users.map((user, key) => {
+
+            return (
+                <div className={`m-widget4__item pointer ${activeUserName === user.get('username') && 'active'}`} key={key}
+                     onClick={() => { this._onUserSelect(user.get('id')) }}>
+                    <div className="m-widget4__img m-widget4__img--pic">
+                        <img src={user.get('avatar')} alt={ user.get('username') }/>
+                    </div>
+                    <div className="m-widget4__info">
+                          <span className="m-widget4__title">
+                          { user.get('username') }
+                          </span><br/>
+                          <span className="m-widget4__sub"></span>
+                    </div>
+                </div>       
+            );
+        });
+    }
 
   render() {
-    const { loading, onSearch } = this.props;
+    const { loading, onSearch, t } = this.props;
 
     return (
       <div className="m-widget1 m-widget1--paddingless">
         <span className="m-list-search__result-category m-list-search__result-category--first">
-          Users
+            {t('users')}
         </span>
         <div className="m-input-icon m-input-icon--right">
           <MetronicSearchInput
             className="form-control m-input"
             id="search"
             type='search'
-            placeholder="Search..."
+            placeholder={t('searchUser')}
             onChange={onSearch}/>
           <span className="m-input-icon__icon m-input-icon__icon--right">
             {loading ? (
@@ -87,4 +87,4 @@ class UserBrowser extends Component {
   }
 }
 
-export default UserBrowser;
+export default translate('translations')(UserBrowser);
