392ccb019eb866da8186ab70812904c97194504a
Pavel Kamenev <pkamenev@gmail.com>
2018-11-17 11:09:15 +0600

    transactions

--- src/redux/transactions/reducer.js
+++ src/redux/transactions/reducer.js
@@ -4,53 +4,44 @@ import {
 import Immutable from 'immutable';
 
 const initialState = Immutable.fromJS({
-  getRecordsRequest: {
-    loading: false,
-    success: false,
-    fail: false,
-    errorResponse: null
-  },
-
-  pagination: {
-    page: 1,
-    perPage: 25,
-    total: 0,
-    totalPages: 1
-  },
-  records: []
+    getRecordsRequest: {
+        loading: false,
+        success: false,
+        fail: false,
+        errorResponse: null,
+        records: [],
+        pagination: {
+            page: 1,
+            perPage: 25,
+            total: 0,
+            totalPages: 1
+        }    
+    }
 });
 
 export default function reducer (state = initialState, action) {
-  switch(action.type) {
-    /**
-     * Get records
-     */
-    case GET_RECORDS:
-      return state
-        .set('getRecordsRequest', state.get('getRecordsRequest')
-          .set('loading', true)
-          .remove('success')
-          .remove('fail')
-        ).set('records', Immutable.List());
-    case GET_RECORDS_SUCCESS:
-      return state
-        .set('getRecordsRequest', state.get('getRecordsRequest')
-          .set('success', true)
-          .remove('loading')
-        ).set('records', Immutable.fromJS(action.result.data))
-        .set('pagination', Immutable.fromJS(action.result.meta.pagination));
-    case GET_RECORDS_FAIL:
-      return state
-        .set('getRecordsRequest', state.get('getRecordsRequest')
-          .set('loading', false)
-          .set('fail', true)
-        );
+    switch(action.type) {
+        /**
+         * Get records
+         */
+        case GET_RECORDS:
+            return state.set('getRecordsRequest', initialState.get('getRecordsRequest').set('loading', true).set('records', Immutable.List()));
+        case GET_RECORDS_SUCCESS:
+            return state
+                .set('getRecordsRequest', state.get('getRecordsRequest')
+                  .set('success', true)
+                  .set('pagination', Immutable.fromJS(action.result.meta.pagination))
+                  .set('records', Immutable.fromJS(action.result.data))
+                  .remove('loading')
+                );
+        case GET_RECORDS_FAIL:
+            return state.set('getRecordsRequest', initialState.get('getRecordsRequest').set('fail', true));
 
 
-    /**
-     * default
-     */
-    default:
-      return state;
-  }
+        /**
+         * default
+         */
+        default:
+            return state;
+    }
 }
