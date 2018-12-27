2dadc0f99559bbe3d624d6e2d19a0d9a16784944
Pavel Kamenev <pkamenev@gmail.com>
2018-12-27 11:40:13 +0600

    splash slider

---
+++ src/containers/static/sections/SplashSlider.js
@@ -0,0 +1,94 @@
+import React, {PureComponent} from 'react';
+import {connect} from 'react-redux';
+import {selectGetRecordsRequest, selectRecords} from "../../../redux/store/selectors";
+import {getRecords} from "../../../redux/store/actions";
+import ProductCard from "../../../components/store/ProductCard";
+import Slider from "react-slick";
+
+class SplashSlider extends PureComponent {
+
+    componentDidMount() {
+        this.props.getRecords()
+    }
+
+    _renderSlideItems(records) {
+        return records.map((item, i) => {
+            return (
+                <div key={i}>
+                    <ProductCard type="vertical" data={item}/>
+                </div>);
+            });
+        }
+
+    _renderSlider() {
+        const {records, t} = this.props;
+        
+        if (records.size === 0 ) {
+            return <div className='text-center'>
+                <h4 className='m--margin-60'>{t('productsNotFound')}</h4>
+            </div>;
+        }
+    
+        const settings = {
+            className: "center",
+            centerMode: true,
+            infinite: true,
+            centerPadding: '0',
+            slidesToShow: 5,
+            speed: 500,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        fade: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        };
+        return (
+            <Slider {...settings}>
+                {this._renderSlideItems(records)}
+            </Slider>
+        );
+    }
+    
+    render() {
+        const {t, getRecordsRequest} = this.props;
+        return (
+            <div className="splash-store">                
+                <h2 className="text-center m--margin-bottom-25">{t('bookstore')}</h2>
+                <section className="productSection">     
+                    <div className="storeSlider m--margin-30">
+                        {getRecordsRequest.get('success') && this._renderSlider() }
+                    </div>                
+                </section>
+            </div>
+        )
+    }
+}
+
+SplashSlider = connect(
+  (state) => ({
+    getRecordsRequest: selectGetRecordsRequest(state),
+    records: selectRecords(state),
+  }),
+  (dispatch) => ({
+    getRecords: (params = {perPage: '50', filter: {category: 1}}) => {
+        dispatch(getRecords(params))
+    }
+  })
+)(SplashSlider);
+
+export default SplashSlider
+
+
