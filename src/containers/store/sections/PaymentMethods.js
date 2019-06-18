698a9bdca0f4f7028bbe3f742584ae8b4f8da205
Pavel Kamenev <pkamenev@gmail.com>
2019-06-18 13:43:33 +0600

    fixes

--- src/containers/store/sections/PaymentMethods.js
+++ src/containers/store/sections/PaymentMethods.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {withTranslation} from 'react-i18next';
-import {CircularProgress} from '@material-ui/core';
 
 class PaymentMethods extends Component {
   static propTypes = {
